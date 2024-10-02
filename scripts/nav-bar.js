ec86cc1441c479318be9d2b067db38343310faa3
mmnagrampa <160197039+mmnagrampa@users.noreply.github.com>
2024-10-01 22:51:51 -0700

    Delete scripts directory

--- scripts/nav-bar.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Toggle profile dropdown
-    const profileIcon = document.querySelector('.profile-icon');
-    const profileDropdown = document.getElementById('subMenu');
-
-    profileIcon.addEventListener('click', function() {
-        profileDropdown.classList.toggle('active');
-    });
-});
